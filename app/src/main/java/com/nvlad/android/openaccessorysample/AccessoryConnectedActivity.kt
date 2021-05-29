8b78c767521790a8371c1ff755b16e5029a43886
nvlad1 <nvlad1@ukr.net>
2021-05-29 09:25:35 +0300

    development

--- app/src/main/java/com/nvlad/android/openaccessorysample/AccessoryConnectedActivity.kt
+++ app/src/main/java/com/nvlad/android/openaccessorysample/AccessoryConnectedActivity.kt
@@ -14,16 +14,14 @@ class AccessoryConnectedActivity : Activity() {
         Log.d(TAG, "onResume")
         Log.d(TAG, "intent Action: ${intent.action}")
         val intent = intent
-        if (intent != null) {
-            if (intent.action == UsbManager.ACTION_USB_ACCESSORY_ATTACHED) {
-                val usbAccessory = intent.getParcelableExtra<Parcelable>(UsbManager.EXTRA_ACCESSORY)
-                Log.d(TAG, "accessory: $usbAccessory")
-                val redirectIntent = Intent(UsbAccessoryManager.ACTION_USB_ACCESSORY_ATTACHED)
-                redirectIntent.putExtra(UsbManager.EXTRA_ACCESSORY, usbAccessory)
-                redirectIntent.putExtra(UsbManager.EXTRA_PERMISSION_GRANTED, true)
-                Log.d(TAG, "sending broadcast intent for accessory")
-                sendBroadcast(redirectIntent)
-            }
+        if (intent != null && intent.action == UsbManager.ACTION_USB_ACCESSORY_ATTACHED) {
+            val usbAccessory = intent.getParcelableExtra<Parcelable>(UsbManager.EXTRA_ACCESSORY)
+            Log.d(TAG, "accessory: $usbAccessory")
+            val redirectIntent = Intent(UsbAccessoryManager.ACTION_USB_ACCESSORY_ATTACHED)
+            redirectIntent.putExtra(UsbManager.EXTRA_ACCESSORY, usbAccessory)
+            redirectIntent.putExtra(UsbManager.EXTRA_PERMISSION_GRANTED, true)
+            Log.d(TAG, "sending broadcast intent for accessory")
+            sendBroadcast(redirectIntent)
         }
         // Close the activity
         Log.d(TAG, "сlosing activity")
